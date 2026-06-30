544433cbd8fcbd460123217d995a4a99220e2b9f
Greymetre Developer <greymetre@gmail.com>
2026-06-30 08:21:35 +0000

    Changes in user target

--- app/Exports/SalesTargetUsersTemplate.php
+++ app/Exports/SalesTargetUsersTemplate.php
@@ -20,7 +20,7 @@ class SalesTargetUsersTemplate implements FromCollection,WithHeadings,ShouldAuto
 
     public function headings(): array
     {
-        return [['User Id', 'Branch Id', 'User Name', 'Type','04/25','05/25','06/25','07/25','08/25','09/25','10/25','11/25','12/25','01/26','02/26','03/26'],['','','','Add primary or secondary value only.please remove this row before upload.']];
+        return [['User Id', 'Branch Id', 'User Name', 'Type','01/26','01/26_qty','02/26','02/26_qty','03/26','03/26_qty','04/26','04/26_qty','05/26','05/26_qty','06/26','06/26_qty','07/26','07/26_qty','08/26','08/26_qty','09/26','09/26_qty','10/26','10/26_qty','11/26','11/26_qty','12/26','12/26_qty'],['','','','Add primary or secondary value only.please remove this row before upload.']];
     }
 
 }
